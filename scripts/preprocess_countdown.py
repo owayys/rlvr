b8ce715709fb42abf878f84407dbef3489e0ba58
owayys <owais.ahsan@hotmail.com>
2026-03-19 15:09:34 +0500

    add qwen-instruct template support

--- scripts/preprocess_countdown.py
+++ scripts/preprocess_countdown.py
@@ -12,13 +12,32 @@ def make_prefix(dp, template_type):
     target = dp["target"]
     numbers = dp["nums"]
     if template_type == "base":
-        prefix = f"""A conversation between User and Assistant. The user asks a question, and the Assistant solves it. The assistant first thinks about the reasoning process in the mind and then provides the user with the answer.
-User: Using the numbers {numbers}, create an equation that equals {target}. You can use basic arithmetic operations (+, -, *, /) and each number can only be used once. Show your work in <think> </think> tags. And return the final answer in <answer> </answer> tags, for example <answer> (1 + 2) / 3 </answer>.
-Assistant: Let me solve this step by step.
-<think>"""
-    else:
-        raise ValueError(f"Unknown template type: {template_type}")
-    return prefix
+        return (
+            "A conversation between User and Assistant. The user asks a question, "
+            "and the Assistant solves it. The assistant first thinks about the reasoning "
+            "process in the mind and then provides the user with the answer.\n"
+            f"User: Using the numbers {numbers}, create an equation that equals {target}. "
+            "You can use basic arithmetic operations (+, -, *, /) and each number can only "
+            "be used once. Show your work in <think> </think> tags. And return the final "
+            "answer in <answer> </answer> tags, for example <answer> (1 + 2) / 3 </answer>.\n"
+            "Assistant: Let me solve this step by step.\n"
+            "<think>"
+        )
+    if template_type == "qwen-instruct":
+        return (
+            "<|im_start|>system\n"
+            "You are a helpful assistant. You first thinks about the reasoning process in the mind "
+            "and then provides the user with the answer.<|im_end|>\n"
+            "<|im_start|>user\n"
+            f" Using the numbers {numbers}, create an equation that equals {target}. "
+            "You can use basic arithmetic operations (+, -, *, /) and each number can only be used once. "
+            "Show your work in <think> </think> tags. And return the final answer in <answer> </answer> tags, "
+            "for example <answer> (1 + 2) / 3 </answer>.<|im_end|>\n"
+            "<|im_start|>assistant\n"
+            "Let me solve this step by step.\n"
+            "<think>"
+        )
+    raise ValueError(f"unknown template_type: {template_type}")
 
 
 def main():
